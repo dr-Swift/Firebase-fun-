43fb244756992558e9ebf6efc706b7af1dcbb6bc
Ståle Olsen <stale.olsen@kleppnett.no>
2018-06-28 22:31:42 +0200

    Initial Commit

---
+++ Firebase-fun!/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Firebase-fun!
+//
+//  Created by Eskil Olsen on 28.06.2018.
+//  Copyright © 2018 Ståle Olsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
